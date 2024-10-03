63ab926265eb86254f4eef3205c0fb51030af28e
Alisson Moretto <33207993+A1S0N@users.noreply.github.com>
2024-10-03 18:51:42 -0300

    Improved banner

--- utils/banner.go
+++ utils/banner.go
@@ -1,23 +1,64 @@
 package utils
 
 import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+
 	"github.com/projectdiscovery/gologger"
 )
 
-var banner = `
+const (
+	primaryColor   = "\033[0;36m" // Cyan
+	secondaryColor = "\033[0;35m" // Magenta
+	reset          = "\033[0m"
+)
 
-╭╮╱╭┳━━━┳━━━╮╱╱╱╱╱╭━━━╮╱╱╭╮╱╱╱╱╱╱╱╱╭╮
-┃┃╱┃┃╭━━┻╮╭╮┃╱╱╱╱╱┃╭━━╯╱╭╯╰╮╱╱╱╱╱╱╭╯╰╮
-┃╰━╯┃╰━━╮┃┃┃┣━╮╭━━┫╰━━┳╮┣╮╭╋━┳━━┳━┻╮╭╋━━┳━╮
-┃╭━╮┃╭━━╯┃┃┃┃╭╮┫━━┫╭━━┻╋╋┫┃┃╭┫╭╮┃╭━┫┃┃╭╮┃╭╯
-┃┃╱┃┃╰━━┳╯╰╯┃┃┃┣━━┃╰━━┳╋╋┫╰┫┃┃╭╮┃╰━┫╰┫╰╯┃┃
-╰╯╱╰┻━━━┻━━━┻╯╰┻━━┻━━━┻╯╰┻━┻╯╰╯╰┻━━┻━┻━━┻╯
-`
+var glitchTitle = []string{
+	"██╗  ██╗███████╗██████╗ ██╗  ██╗",
+	"██║  ██║██╔════╝██╔══██╗╚██╗██╔╝",
+	"███████║█████╗  ██║  ██║ ╚███╔╝ ",
+	"██╔══██║██╔══╝  ██║  ██║ ██╔██╗ ",
+	"██║  ██║███████╗██████╔╝██╔╝ ██╗",
+	"╚═╝  ╚═╝╚══════╝╚═════╝ ╚═╝  ╚═╝",
+}
+
+var subtitle = "HEDnsExtractor - HuntDownProject"
 
 var version = "v1.0.7"
 
 func ShowBanner() {
-	gologger.Print().Msgf("%s\n", banner)
-	gologger.Info().Msgf("Current hednsextractor version %s", version)
-	gologger.Info().Msgf("HEDnsExtractor Config Directory: %s", configDir)
+	displayGlitchTitle()
+	fmt.Println()
+	gologger.Info().Msgf("Current version: %s", version)
+	gologger.Info().Msgf("Config Directory: %s", configDir)
+}
+
+func displayGlitchTitle() {
+	glitchChars := "!@#$%^&*()_+-=[]{}|;:,.<>?"
+	rand.Seed(time.Now().UnixNano())
+
+	for _, line := range glitchTitle {
+		glitchedLine := ""
+		for _, char := range line {
+			if rand.Float32() < 0.1 { // 10% chance of glitch
+				glitchedLine += string(glitchChars[rand.Intn(len(glitchChars))])
+			} else {
+				glitchedLine += string(char)
+			}
+		}
+
+		if rand.Float32() < 0.5 {
+			fmt.Println(primaryColor + glitchedLine + reset)
+		} else {
+			fmt.Println(secondaryColor + glitchedLine + reset)
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+
+	// Display subtitle
+	fmt.Println(primaryColor + strings.Repeat("=", len(subtitle)) + reset)
+	fmt.Println(secondaryColor + subtitle + reset)
+	fmt.Println(primaryColor + strings.Repeat("=", len(subtitle)) + reset)
 }
